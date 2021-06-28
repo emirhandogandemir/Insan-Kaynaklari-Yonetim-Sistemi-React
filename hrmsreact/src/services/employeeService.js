d0c1ddf339b541cc4ac194542b6ad4621bad90ad
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-06-28 13:47:13 +0300

    Employere Service update,delete,add,getById added

--- hrmsreact/src/services/employeeService.js
+++ hrmsreact/src/services/employeeService.js
@@ -6,4 +6,18 @@ export default class EmployeeService{
     getEmployers(){
         return axios.get("http://localhost:8080/api/employees/getall");
     }
+
+    add(values){
+        return axios.post("http://localhost:8080/api/employees/add",values)
+    }
+    update(values){
+        return axios.post("http://localhost:8080/api/employees/update",values)
+    }
+
+    getById(id){
+return axios.get("http://localhost:8080/api/employees/getbyid?id="+id)
+    }
+delete(id){
+    return axios.delete("http://localhost:8080/api/employees/delete?id="+id)
+}
 }
