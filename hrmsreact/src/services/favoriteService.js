3967120b1dde24305ee39e94ed0f75cb9ad625b2
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-07-03 10:12:47 +0300

    methods has written in favorite service

--- hrmsreact/src/services/favoriteService.js
+++ hrmsreact/src/services/favoriteService.js
@@ -3,7 +3,26 @@ import axios from "axios"
 
 export default class FavoriteService{
 
+add(values){
+    return axios.post("http://localhost:8080/api/favorites/add",values)
+}
+update(values){
+    return axios.post("http://localhost:8080/api/favorites/update",values)
+}
 
+delete(id){
+    return axios.delete("http://localhost:8080/api/favorites/delete?id="+id)
+}
 
+getAll(){
+    return axios.get("http://localhost:8080/api/favorites/getall")
+}
 
+getByJobSeekerId(id){
+return axios.get("http://localhost:8080/api/favorites/getByJobSeekerId?id="+id)
+}
+
+getByJobAdvertId(id){
+    return axios.get("http://localhost:8080/api/favorites/getByAdvertId?id="+id)
+}
 }
